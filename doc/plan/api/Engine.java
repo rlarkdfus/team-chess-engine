3a62e21e0c34f8706ce02247a568ea9d68650168
Samuel Li <samuelli.pk@gmail.com>
2021-11-08 22:01:09 -0500

    added APIs to folder

---
+++ doc/plan/api/Engine.java
@@ -0,0 +1,20 @@
+package ooga.Model;
+
+public interface Engine {
+    /**
+     * Create default board of pieces
+     */
+    void initializeBoard();
+
+    /**
+     * Moves piece from start to end and updates the board
+     * @param start is piece initial location
+     * @param end is piece new location
+     */
+    void movePiece(Location start, Location end);
+
+    /**
+     * Determine whether the win condition of the game is satisfied, and declare a winner.
+     */
+    boolean gameFinished();
+}
---
+++ doc/plan/api/View.java
@@ -0,0 +1,8 @@
+package ooga.View;
+
+public interface View {
+    /**
+     * Updates position of pieces on the board
+     */
+    void updateDisplay();
+}
