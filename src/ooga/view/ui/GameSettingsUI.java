16d044334fc4b64d130cb9941eaf62dc1163524b
Richard Deng <rld39@duke.edu>
2021-11-11 21:33:44 -0500

    worked on GameSettingsUI

--- src/ooga/view/ui/GameSettingsUI.java
+++ src/ooga/view/ui/GameSettingsUI.java
@@ -1,6 +1,21 @@
 package ooga.view.ui;
 
 import javafx.scene.layout.GridPane;
+import ooga.view.util.ViewUtility;
 
-public class GameSettingsUI extends GridPane {
+public class GameSettingsUI extends GridPane implements UIInterface {
+    public GameSettingsUI() {
+        createUI();
+    }
+
+    @Override
+    public void createUI() {
+        this.add(ViewUtility.makeLabel("black_time_label"), 0, 0);
+        this.add(ViewUtility.makeText("black_timer_display"), 0, 1);
+        this.add(ViewUtility.makeLabel("move_history_label"), 0, 2);
+        this.add(ViewUtility.makeGridPane("move_history_gridpane"), 0, 3);
+        this.add(ViewUtility.makeButton("download_game", e -> System.out.println("download game")), 0, 4);
+        this.add(ViewUtility.makeLabel("white_time_label"), 0, 5);
+        this.add(ViewUtility.makeText("white_timer_display"), 0, 6);
+    }
 }
--- src/ooga/view/util/ViewUtility.java
+++ src/ooga/view/util/ViewUtility.java
@@ -5,8 +5,10 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
+import javafx.scene.text.Text;
 
 import java.util.HashMap;
 import java.util.List;
@@ -24,10 +26,10 @@ public class ViewUtility {
      * @return the label we made
      */
     public static Label makeLabel(String property) {
-        Label label = new Label();
-        label.setText(myResources.getString(property));
-        //label.setFont(Font.font("Arial", FontWeight.BOLD, 24));
-        return (Label) setID(property, label);
+        Label result = new Label();
+        result.setText(myResources.getString(property));
+        //result.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+        return (Label) setID(property, result);
     }
 
     /**
@@ -44,22 +46,30 @@ public class ViewUtility {
         return (Button) setID(property, result);
     }
 
+    // used for timers, think of better way maybe?
+    public static Text makeText(String property) {
+        Text result = new Text(property);
+        result.setText(myResources.getString(property));
+        result.getStyleClass().add("text");
+        return (Text) setID(property, result);
+    }
+
     /**
      *
      * @param response the event to occur on action with the combobox
      * @return the combobox that was made
      */
     public static ComboBox makeComboBox(String property, List<String> choices, Consumer<String> response) {
-        ComboBox box = new ComboBox();
+        ComboBox result = new ComboBox();
         Map<String, String> lang = new HashMap<>();
         for (String option : choices) {
             lang.put(myResources.getString(option), option);
         }
-        box.setItems(FXCollections.observableArrayList(lang.keySet().stream().toList()));
-        box.valueProperty()
+        result.setItems(FXCollections.observableArrayList(lang.keySet().stream().toList()));
+        result.valueProperty()
                 .addListener((o, oldValue, newValue) -> response.accept(lang.get(newValue)));
-        box.getStyleClass().add("combo-box");
-        return (ComboBox) setID(property, box);
+        result.getStyleClass().add("combo-box");
+        return (ComboBox) setID(property, result);
     }
 
     /**
@@ -70,15 +80,20 @@ public class ViewUtility {
      * @return the slider
      */
     public static Slider makeSlider(String property, double min, double max, double start, Consumer<Number> response) {
-        Slider slider = new Slider(min, max, start);
-        slider.valueProperty().addListener((o, oldValue, newValue) -> response.accept(newValue));
-        slider.getStyleClass().add("slider");
-        return (Slider) setID(property, slider);
+        Slider result = new Slider(min, max, start);
+        result.valueProperty().addListener((o, oldValue, newValue) -> response.accept(newValue));
+        result.getStyleClass().add("slider");
+        return (Slider) setID(property, result);
     }
 
     public static ColorPicker makeColorPicker(String property, Color color) {
-        ColorPicker picker = new ColorPicker(color);
-        return (ColorPicker) setID(property, picker);
+        ColorPicker result = new ColorPicker(color);
+        return (ColorPicker) setID(property, result);
+    }
+
+    public static GridPane makeGridPane(String property) {
+        GridPane result = new GridPane();
+        return (GridPane) setID(property, result);
     }
 
     /**
