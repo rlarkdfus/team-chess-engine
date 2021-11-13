68da994c930c77e756218a452a1ebd876964f2c2
Luis Pereda Amaya <lip4@duke.edu>
2021-11-13 15:36:31 -0500

    Tested to make sure LocationParserwrites location data correctly

---
+++ test/ooga/controller/LocationParserTest.java
@@ -0,0 +1,35 @@
+package ooga.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LocationParserTest {
+
+  private LocationParser locationParser;
+  private String filePath;
+
+  @BeforeEach
+  void setUp() {
+    locationParser =  new LocationParser();
+    filePath = "./data/chess/locations/chess.csv";
+  }
+
+  @Test
+  void testLocationParserWritesCorrectly() throws Exception {
+    List<List<String>> initialPositions = locationParser.getInitialLocations(filePath);
+    String expected = "b_bishop";
+    String test =  initialPositions.get(0).get(2);
+    assertEquals(expected, test);
+  }
+
+  @Test
+  void testZerosWrittenAtEmptyPositions() throws Exception {
+    List<List<String>> initialPositions = locationParser.getInitialLocations(filePath);
+    String expected = "0";
+    String test =  initialPositions.get(4).get(3);
+    assertEquals(expected, test);
+  }
+}
