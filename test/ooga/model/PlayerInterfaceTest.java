d49c2dcfe8fbc89b967e76748b31c80940b2dd42
Samuel Li <samuelli.pk@gmail.com>
2021-11-14 22:09:37 -0500

    added moveVector tests and fixed piece/playerinterface tests

--- test/ooga/model/PlayerInterfaceTest.java
+++ test/ooga/model/PlayerInterfaceTest.java
@@ -1,51 +1,53 @@
-//package ooga.model;
-//
-//import org.assertj.core.api.Assert;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class PlayerInterfaceTest {
-//    private PlayerInterface player;
-//    private PieceInterface piece;
-//    @BeforeEach
-//    void setUp() {
-//        player = new Player("white");
-//        List<Piece.Vector> vectors = new ArrayList<>();
-//        vectors.add(new Piece.Vector(-1, 0));
-//        vectors.add(new Piece.Vector(1, 0));
-//        vectors.add(new Piece.Vector(0, 1));
-//        vectors.add(new Piece.Vector(0, -1));
-//
-//
-//        piece = new Piece("white", vectors, false);
-//        player.addPiece(piece);
-//    }
-//
-//    @Test
-//    void removePiece() {
-//        player.removePiece((Piece) piece);
-//        Assertions.assertNull(player.getPieces());
-//    }
-//
-//    @Test
-//    void getPieces() {
-//        Assertions.assertNotNull(player.getPieces());
-//    }
-//
-//    @Test
-//    void addPiece() {
-//        player.addPiece(piece);
-//        Assertions.assertEquals(player.getPieces().size(), 2);
-//    }
-//
-//    @Test
-//    void getTeam() {
-//        Assertions.assertEquals(player.getTeam(), "white");
-//    }
-//}
+package ooga.model;
+
+import ooga.Location;
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerInterfaceTest {
+    private PlayerInterface player;
+    private PieceInterface piece;
+    @BeforeEach
+    void setUp() {
+        player = new Player("white");
+        List<Vector> vectors = new ArrayList<>();
+        vectors.add(new Vector(-1, 0));
+        vectors.add(new Vector(1, 0));
+        vectors.add(new Vector(0, 1));
+        vectors.add(new Vector(0, -1));
+
+        Location loc = new Location(6, 0);
+        MoveVector vec = new MoveVector(vectors, vectors, vectors);
+        piece = new Piece("w", "P", loc, vec, false);
+        player.addPiece(piece);
+    }
+
+    @Test
+    void removePiece() {
+        player.removePiece(piece.getLocation());
+        Assertions.assertEquals(player.getPieces().size(), 0);
+    }
+
+    @Test
+    void getPieces() {
+        Assertions.assertNotNull(player.getPieces());
+    }
+
+    @Test
+    void addPiece() {
+        player.addPiece(piece);
+        Assertions.assertEquals(player.getPieces().size(), 2);
+    }
+
+    @Test
+    void getTeam() {
+        Assertions.assertEquals(player.getTeam(), "white");
+    }
+}
