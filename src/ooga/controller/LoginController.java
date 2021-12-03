3a4928da2f4584fd6eec3bdbf49d2f5855eb3077
Luis Pereda Amaya <lip4@duke.edu>
2021-12-03 17:00:52 -0500

    incorrect password being added

--- src/ooga/controller/LoginController.java
+++ src/ooga/controller/LoginController.java
@@ -13,11 +13,13 @@ public class LoginController {
         loginView.initializeDisplay();
     }
 
-    public void handleLoginAttempt(String username, String password) throws Exception {
-        if (isValidLogin(username, password)) {
+    public boolean handleLoginAttempt(String username1, String password1, String username2, String password2) throws Exception {
+        if (isValidLogin(username1, password1) && isValidLogin(username2, password2)) {
             new Controller();
-
+            hideLoginView();
+            return true;
         }
+        return false;
     }
 
     public void handleGuestLogin() {
@@ -29,7 +31,6 @@ public class LoginController {
     }
 
     private boolean isValidLogin(String username, String password) throws Exception {
-        System.out.println("validating login");
         try {
             jsonParser = new JsonParser();
             File userFile = new File(String.format("data/chess/profiles/%s.json", username));
@@ -38,7 +39,6 @@ public class LoginController {
             return truePassword.equals(password);
         }
         catch (Exception e) {
-            System.out.println("invalid login");
             throw new Exception("Invalid username. Please login with an existing username or sign in to create an account");
         }
     }
--- src/ooga/view/ui/loginUI/LoginUI.java
+++ src/ooga/view/ui/loginUI/LoginUI.java
@@ -1,5 +1,6 @@
 package ooga.view.ui.loginUI;
 
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
@@ -13,6 +14,7 @@ public class LoginUI extends GridPane implements UIInterface {
 
     LoginController loginController;
     ViewUtility viewUtility;
+    Label incorrectPassword;
 
     public LoginUI(LoginController loginController) {
         this.loginController = loginController;
@@ -26,7 +28,7 @@ public class LoginUI extends GridPane implements UIInterface {
         this.add(viewUtility.makeLabel("welcome"), 0, 0, 1, 1);
         this.add(viewUtility.makeLabel("blackTeam"), 0, 1, 1, 1);
         this.add(viewUtility.makeLabel("username"), 0, 2, 1, 1);
-        this.add(viewUtility.makeTextField("username_field",  e -> {
+        this.add(viewUtility.makeTextField("black_username_field",  e -> {
             try {
                 handleKeyPressed(e);
             } catch (Exception ex) {
@@ -34,7 +36,7 @@ public class LoginUI extends GridPane implements UIInterface {
             }
         }), 0, 3, 3, 1);
         this.add(viewUtility.makeLabel("password"), 0, 4, 3, 1);
-        this.add(viewUtility.makePasswordField("password_field", e -> {
+        this.add(viewUtility.makePasswordField("black_password_field", e -> {
             try {
                 handleKeyPressed(e);
             } catch (Exception ex) {
@@ -43,7 +45,7 @@ public class LoginUI extends GridPane implements UIInterface {
         }), 0, 5, 3, 1);
         this.add(viewUtility.makeLabel("whiteTeam"), 0, 6, 1, 1);
         this.add(viewUtility.makeLabel("username"), 0, 7, 1, 1);
-        this.add(viewUtility.makeTextField("username_field",  e -> {
+        this.add(viewUtility.makeTextField("white_username_field",  e -> {
             try {
                 handleKeyPressed(e);
             } catch (Exception ex) {
@@ -51,7 +53,7 @@ public class LoginUI extends GridPane implements UIInterface {
             }
         }), 0, 8, 3, 1);
         this.add(viewUtility.makeLabel("password"), 0, 9, 1, 1);
-        this.add(viewUtility.makePasswordField("password_field", e -> {
+        this.add(viewUtility.makePasswordField("white_password_field", e -> {
             try {
                 handleKeyPressed(e);
             } catch (Exception ex) {
@@ -70,6 +72,9 @@ public class LoginUI extends GridPane implements UIInterface {
     }
 
     private void handleKeyPressed(KeyEvent e) throws Exception {
+        if (this.getChildren().contains(incorrectPassword)) {
+            this.getChildren().remove(incorrectPassword);
+        }
         if (e.getCode() == KeyCode.ENTER) {
             handleLoginAction();
         }
@@ -80,8 +85,16 @@ public class LoginUI extends GridPane implements UIInterface {
     }
 
     private void handleLoginAction() throws Exception {
-        String username = ((TextField) lookup("#username_field")).getText();
-        String password = ((PasswordField) lookup("#password_field")).getText();
-        loginController.handleLoginAttempt(username, password);
+        String whiteUsername = ((TextField) lookup("#white_username_field")).getText();
+        String whitePassword = ((PasswordField) lookup("#white_password_field")).getText();
+        String blackUsername = ((TextField) lookup("#white_username_field")).getText();
+        String blackPassword = ((PasswordField) lookup("#white_password_field")).getText();
+        boolean login = loginController.handleLoginAttempt(blackUsername, blackPassword, whiteUsername, whitePassword);
+        System.out.println(login);
+        if (!login) {
+            incorrectPassword = viewUtility.makeLabel("incorrectPassword");
+            this.add(incorrectPassword, 1, 4, 1, 1);
+
+        }
     }
 }
