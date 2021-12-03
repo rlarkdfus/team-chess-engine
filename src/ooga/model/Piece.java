5cdad07e3e8333dd01f06e8cf37792e50f0c385c
Samuel Li <samuelli.pk@gmail.com>
2021-12-02 20:22:33 -0500

    remove unused files

--- src/ooga/model/Piece.java
+++ src/ooga/model/Piece.java
@@ -28,15 +28,6 @@ public class Piece implements PieceInterface {
 
   private List<Move> moves;
 
-//  public Piece(String team, String name, Location location, MoveVector moveVectors, Map<String, Boolean> attributes, int score) {
-//    this.location = location;
-//    this.team = team;
-//    this.moved = false;
-//    this.score = score;
-//    this.name = name;
-//    this.attributes = attributes;
-//  }
-  //Todo: make this the default constructor
   public Piece(String team, String name, Location location, List<Move> moves, Map<String, Boolean> attributes, int score) {
     this.location = location;
     this.team = team;
@@ -119,24 +110,6 @@ public class Piece implements PieceInterface {
     }
 
   /**
-   * return the unique id of a piece
-   * @return
-   */
-//  @Override
-//  public int getUniqueId() {
-//    return this.uniqueID;
-//  }
-
-  /**
-   * set a piece to be eliminated
-   * @param state
-   */
-//  @Override
-//  public void setEliminated(boolean state) {
-//    isEliminated = state;
-//  }
-
-  /**
    * override toString to print out piece information
    *
    * @return
@@ -168,9 +141,6 @@ public class Piece implements PieceInterface {
     return Objects.hash(initialLocation, team, score, name);
   }
 
-
-
-
   /**
    * get all the locations a piece can move to
    * @return
@@ -179,7 +149,6 @@ public class Piece implements PieceInterface {
   public List<Location> getEndLocations() {
     List<Location> locations = new ArrayList<>();
     for(Move move : moves) {
-//      System.out.println(move.getClass());
       locations.addAll(move.getEndLocations());
     }
     return locations;
@@ -215,19 +184,6 @@ public class Piece implements PieceInterface {
     this.score = newPiece.getScore();
   }
 
-//  /**
-//   * return all the end locations of a piece
-//   * @return
-//   */
-//  @Override
-//  public List<Location> getAllEndLocations() {
-//    List<Location> endLocations = new ArrayList<>();
-//    for(Move move : moves) {
-//      endLocations.addAll(move.getEndLocations());
-//    }
-//    return endLocations;
-//  }
-
   /**
    * this method returns the move that results in a piece being at the end location
    * @param end
@@ -256,6 +212,10 @@ public class Piece implements PieceInterface {
     }
   }
 
+  /**
+   * return the moves of a piece
+   * @return
+   */
   @Override
   public List<Move> getMoves(){
     return moves;
@@ -271,6 +231,10 @@ public class Piece implements PieceInterface {
     return team.equals(piece.getTeam());
   }
 
+  /**
+   * return whether a piece can transform into other pieces
+   * @return
+   */
   public boolean canTransform(){
     return this.attributes.getOrDefault("canTransform", false);
   }
