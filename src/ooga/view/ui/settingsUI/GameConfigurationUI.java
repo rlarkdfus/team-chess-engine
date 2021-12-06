33ed5249f6619045fa342d79c3c03068bf7c83a8
Samuel Li <samuelli.pk@gmail.com>
2021-12-06 14:08:45 -0500

    preserve piece style after update

--- src/ooga/view/ui/settingsUI/GameConfigurationUI.java
+++ src/ooga/view/ui/settingsUI/GameConfigurationUI.java
@@ -10,6 +10,7 @@ public class GameConfigurationUI extends GridPane implements UIInterface {
 
     private ControllerInterface controller;
     private ViewUtility viewUtility;
+    private final List<String> variations = List.of("Game", "Editor");
 
     public GameConfigurationUI(ControllerInterface controller) {
         this.controller = controller;
@@ -21,7 +22,7 @@ public class GameConfigurationUI extends GridPane implements UIInterface {
     @Override
     public void createUI() {
         this.add(viewUtility.makeLabel("variation"), 0, 0);
-        this.add(viewUtility.makeMenu("game_variation", List.of("standard", "editor"), System.out::println), 1, 0);
+        this.add(viewUtility.makeMenu("game_variation", variations, System.out::println), 1, 0);
         this.add(viewUtility.makeButton("upload_configuration", e -> {controller.uploadConfiguration(viewUtility.selectJSONFile());}), 1, 1);
     }
 }
