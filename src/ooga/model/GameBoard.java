67b26c49c1dc8b9e0a31fffbd6056a549fccc1f5
Samuel Li <samuelli.pk@gmail.com>
2021-12-06 00:42:46 -0500

    updated javadocs of model

--- src/ooga/model/GameBoard.java
+++ src/ooga/model/GameBoard.java
@@ -1,19 +1,30 @@
 package ooga.model;
 
 import ooga.Location;
-import ooga.controller.Config.InvalidPieceConfigException;
 import ooga.model.EndConditionHandler.EndConditionRunner;
 import ooga.model.Moves.Move;
 import ooga.model.Powerups.PowerupInterface;
-
-import java.io.FileNotFoundException;
 import java.util.List;
 
+/**
+ * @authors
+ * purpose - the gameboard is the playable board and keeps track of the pieces, players, and turns
+ * assumptions - it assumes that the pieces and players are all valid
+ * dependencies - it depends on EndConditionRunner, PlayerInterface, PowerupInterface,
+ * Location, PieceInterface, and Move
+ * usage - a user is able to call its methods to get and manage the moves of a piece
+ */
 public class GameBoard extends Board {
-
     private EndConditionRunner endCondition;
     private int turnCount;
 
+    /**
+     * the gameboard initializes the turns and the legal moves of each piece
+     * @param players list of all the players
+     * @param endCondition manages all end conditions
+     * @param powerups list of the powerups
+     * @param bounds bounds of the board
+     */
     public GameBoard(List<PlayerInterface> players, EndConditionRunner endCondition, List<PowerupInterface> powerups, Location bounds) {
         super(players,bounds);
         this.endCondition = endCondition;
@@ -23,15 +34,23 @@ public class GameBoard extends Board {
             piece.updateMoves(pieces);
         }
         updateLegalMoves();
-
-
     }
 
+    /**
+     * get the move that will move a player to the end location
+     * @param end end location for a move
+     * @param piece the piece that is being moved
+     * @return the move that moves a player to the end location
+     */
     @Override
     protected Move getMove(Location end, PieceInterface piece) {
         return piece.getMove(end);
     }
 
+    /**
+     * checks for powerups and toggles timers
+     * @param piece piece to update rules
+     */
     @Override
     protected void updateGameRules(PieceInterface piece) {
         for(PowerupInterface powerupInterface: powerupInterfaceList){
@@ -41,6 +60,11 @@ public class GameBoard extends Board {
         toggleTimers();
     }
 
+    /**
+     * determines whether a piece at a location is eligible to be moved
+     * @param location location that a player selects
+     * @return whether the piece at the location can be moved
+     */
     @Override
     public boolean canMovePiece(Location location) {
         String turn = findPlayerTurn(turnCount).getTeam();
@@ -54,9 +78,8 @@ public class GameBoard extends Board {
 
     /**
      * return a list of all legal moves for a piece at a location
-     *
-     * @param location
-     * @return
+     * @param location selected to move
+     * @return list of all the end locations of the piece at location
      */
     @Override
     public List<Location> getLegalMoves(Location location) {
@@ -84,7 +107,10 @@ public class GameBoard extends Board {
         return players.get((turn) % players.size());
     }
 
-
+    /**
+     * determine the current running game state
+     * @return the current game state
+     */
     @Override
     public GameState checkGameState() {
         return endCondition.satisfiedEndCondition(players);
--- src/ooga/model/PieceInterface.java
+++ src/ooga/model/PieceInterface.java
@@ -6,19 +6,98 @@ import java.util.Map;
 import ooga.Location;
 import ooga.model.Moves.Move;
 
+/**
+ * @authors
+ * purpose - this is the pieceinterface api which provides all the methods for interacting with
+ * the piece
+ * assumptions - it assumes that a piece exists and has valid attributes
+ * dependencies - it does not depend on any other classes
+ * usage - moves and other classes use this api to move the pieces and update their locations
+ */
 public interface PieceInterface {
+    /**
+     * gets the team of a piece
+     * @return the team of a piece
+     */
     String getTeam();
+
+    /**
+     * get the score of a piece
+     * @return score of a piece
+     */
     int getScore();
+
+    /**
+     * get the name of a piece
+     * @return name of a piece
+     */
     String getName();
+
+    /**
+     * get the location of a piece
+     * @return location of a piece
+     */
     Location getLocation();
+
+    /**
+     * move a piece to a location
+     * @param location location to move to
+     */
     void moveTo(Location location);
+
+    /**
+     * test out a move to a location, without updating the move and firstmove booleans
+     * @param location location to move to
+     */
     void tryMove(Location location);
+
+    /**
+     * get all the end locations a piece can move to
+     * @return list of all the end locations
+     */
     List<Location> getEndLocations();
+
+    /**
+     * has a piece been moved before
+     * @return whether a piece has been moved before
+     */
     boolean hasMoved();
+
+    /**
+     * is the previous move a piece's first move
+     * @return whether a piece has just done its first move
+     */
     boolean isFirstMove();
+
+    /**
+     * transform a piece into the newpiece and take on its attributes
+     * @param newPiece piece to transform into
+     */
     void transform(PieceInterface newPiece);
+
+    /**
+     * get the move that moves a piece to end location
+     * @param end location that the move goes to
+     * @return move that goes to the location
+     */
     Move getMove(Location end);
+
+    /**
+     * updates the legal moves of a piece after moving
+     * @param pieces all the pieces
+     */
     void updateMoves(List<PieceInterface> pieces);
+
+    /**
+     * gets the list of all possible moves for a piece
+     * @return the list of all possible moves for a piece
+     */
     List<Move> getMoves();
+
+    /**
+     * determine whether the piece and another piece are on the same team
+     * @param piece the other piece to compare to
+     * @return whether pieces are on the same team
+     */
     boolean isSameTeam(PieceInterface piece);
 }
