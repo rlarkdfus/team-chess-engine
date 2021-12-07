e24cce72c3374d44e4af114ebd4b6238ea1af4a5
Gordon Kim <kk388@duke.edu>
2021-12-06 22:51:43 -0500

    Map ui behavior in Game configuration ui

--- src/ooga/view/ui/settingsUI/GameConfigurationUI.java
+++ src/ooga/view/ui/settingsUI/GameConfigurationUI.java
@@ -1,15 +1,29 @@
 package ooga.view.ui.settingsUI;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import java.util.Map;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.layout.GridPane;
 import ooga.controller.ControllerInterface;
-import ooga.controller.GameController;
 import ooga.view.ui.UIInterface;
 import ooga.view.util.ViewUtility;
 
 public class GameConfigurationUI extends GridPane implements UIInterface {
+
+    public static final String VARIATION_LABEL = "variation";
+    public static final String GAME_VARIATION = "game_variation";
+    public static final String UPLOAD_CONFIGURATION = "upload_configuration";
+    public static final String DOWNLOAD_GAME = "download_game";
+    public static final String NEW_WINDOW = "new_window";
+    public static final String RESET_WINDOW = "reset_window";
+    private final Map<String, EventHandler<ActionEvent>> buttonBehavior = Map.of(
+            UPLOAD_CONFIGURATION, e -> uploadConfiguration(),
+            DOWNLOAD_GAME, e -> downloadGame(),
+            NEW_WINDOW, e -> createNewWindow(),
+            RESET_WINDOW, e -> reset()
+    );
 
     private ControllerInterface controller;
     private ViewUtility viewUtility;
@@ -24,12 +38,12 @@ public class GameConfigurationUI extends GridPane implements UIInterface {
 
     @Override
     public void createUI() {
-        this.add(viewUtility.makeLabel("variation"), 0, 0);
-        this.add(viewUtility.makeMenu("game_variation", variations, e -> chooseVariation(e)), 1, 0);
-        this.add(viewUtility.makeButton("upload_configuration", e -> uploadConfiguration()), 1, 1);
-        this.add(viewUtility.makeButton("download_game", e -> downloadGame()), 2, 1);
-        this.add(viewUtility.makeButton("new_window", e -> createNewWindow()), 2, 2);
-        this.add(viewUtility.makeButton("reset_window", e -> reset()), 1, 2);
+        this.add(viewUtility.makeLabel(VARIATION_LABEL), 0, 0);
+        this.add(viewUtility.makeMenu(GAME_VARIATION, variations, this::chooseVariation), 1, 0);
+        this.add(viewUtility.makeButton(UPLOAD_CONFIGURATION, buttonBehavior.get(UPLOAD_CONFIGURATION)), 1, 1);
+        this.add(viewUtility.makeButton(DOWNLOAD_GAME, buttonBehavior.get(DOWNLOAD_GAME)), 2, 1);
+        this.add(viewUtility.makeButton(NEW_WINDOW, buttonBehavior.get(NEW_WINDOW)), 2, 2);
+        this.add(viewUtility.makeButton(RESET_WINDOW, buttonBehavior.get(RESET_WINDOW)), 1, 2);
     }
 
     private void chooseVariation(String controllerVariation) {
