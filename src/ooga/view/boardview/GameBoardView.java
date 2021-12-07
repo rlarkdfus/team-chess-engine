6c4d47c895b4bdf9ca3f64f6b8e3784f9cf3ff34
Luis Pereda Amaya <lip4@duke.edu>
2021-12-07 02:53:09 -0500

    JavaDocs

--- src/ooga/view/boardview/GameBoardView.java
+++ src/ooga/view/boardview/GameBoardView.java
@@ -5,15 +5,34 @@ import ooga.Location;
 import ooga.controller.Config.PieceViewBuilder;
 import ooga.controller.GameControllerInterface;
 
+/**
+ * Purpose: Handles the complexities of the UI needed to represent the board in a game of chess
+ * Assumptions: The controller will correctly provide the class with the information needed for its
+ * functionality.
+ * Dependencies: PieceViewBuilder, GameControllerInterface, Location, GameControllerInterface
+ * @author Gordon Kim
+ */
 public class GameBoardView extends BoardView {
 
     private GameControllerInterface controller;
 
+    /**
+     * Create a new GameBoardView
+     * @param controller Controller needed to handle game board interactions
+     * @param pieceViews List of view representations of the pieces
+     * @param row Row number
+     * @param col Column number
+     */
     public GameBoardView(GameControllerInterface controller, List<PieceViewBuilder> pieceViews, int row, int col) {
         super(pieceViews, row, col);
         this.controller = controller;
     }
 
+    /**
+     * Handle the work needed when a click is registered by the board. This helps the user by showing
+     * possible moves and allowing them to move their pieces.
+     * @param clickLocation Location where the click is registered.
+     */
     @Override
     protected void clickBoard(Location clickLocation) {
         Location startLocation = getSelectedLocation();
